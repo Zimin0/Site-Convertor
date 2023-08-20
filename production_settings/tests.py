f57528300b1de84bfdf82ab360c73e2ab03d9f07
Zimin0 <you@example.com>
2023-08-20 22:04:31 +0300

    tests added

--- production_settings/tests.py
+++ production_settings/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from production_settings.models import ProductionSettings
 
-# Create your tests here.
+data = [
+    {
+       'name': 'Почта техподдержки для получения писем',
+       'slug': 'TECH_EMAIL_TO',
+       'value':  'nikzim2004@gmail.com'
+    }, 
+    {
+       'name': 'Ключ приложения от почты gmail',
+       'slug': 'GMAIL_APP_KEY',
+       'value': 'value' 
+    }, 
+    {
+       'name': 'Цена в рублях',
+       'slug': 'RUBLE_PRICE',
+       'value': '310' 
+    },     {
+       'name': 'Кол-во бесплатных конвертаций',
+       'slug': 'CONVERT_AMOUNT',
+       'value': '2' 
+    },     {
+       'name': 'Почта техподдержки для отправки писем',
+       'slug': 'TECH_EMAIL_FROM',
+       'value': 'nikzim2004@gmail.com' 
+    }, 
+]
+
+for setting in data:
+    exists_in_db = ProductionSettings.objects.filter(name=setting['name']).exists()
+    
+    if not exists_in_db:
+        print(f"Настройка '{setting['name']}' еще не добавлена!")
+        s = ProductionSettings.objects.create(
+            name=setting['name'],
+            slug=setting['slug'],
+            value=setting['value'],
+        )
+        s.save()
+    else:
+        print(f"Настройка {setting['name']} уже существует!")
--- users/tests.py
+++ users/tests.py
@@ -1,8 +1,8 @@
-from django.test import TestCase
+# from django.test import TestCase
 
-from users.models import Profile
-from django.contrib.auth.models import User
+# from users.models import Profile
+# from django.contrib.auth.models import User
 
-admin = User.objects.all().first()
-g = Profile.objects.create(user=admin, phone='+79313661220')
-g.save()
+# admin = User.objects.all().first()
+# g = Profile.objects.create(user=admin, phone='+79313661220')
+# g.save()
